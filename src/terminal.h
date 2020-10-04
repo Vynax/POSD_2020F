df4e9f42650adfcf71b14a589b50b81bd60de41e
dell_wsl_ubuntu20.04 <t109598085@ntut.org.tw>
2020-10-04 16:20:55 +0800

    HW3_stuck_at_regex

--- src/terminal.h
+++ src/terminal.h
@@ -5,14 +5,15 @@ class Terminal
 public:
     Terminal(std::string input)
     {
-        std::string shapep("(Rectangle|Ellipse|Triangle)"); //shape pattern
-        std::string spacep(" ");                            // space pattern
+        std::string shape("(Rectangle|Ellipse|Triangle)"); //shape pattern
+        std::string space_plus(" +");                      // space pattern
+        std::string space(" ");
         std::string lparenthesis("\(");
-        std::string decimal_point("[0-9]+(.[0-9]*)?"); //((Rectangle|Ellipse|Triangle) \( *[0-9]+(.[0-9]*)?, *[0-9]+(.[0-9]*)?\))|(Triangle) \(\[-?[0-9]+(.[0-9]*)? *, *-?[0-9]+(.[0-9]*)?
+        std::string decimalN("-?[0-9]+(.[0-9]*)?"); //((Rectangle|Ellipse|Triangle) \( *[0-9]+(.[0-9]*)?, *[0-9]+(.[0-9]*)?\))|(Triangle) \(\[-?[0-9]+(.[0-9]*)? *, *-?[0-9]+(.[0-9]*)?
         std::string str;
         std::regex pattern;
 
-        str = shapep + spacep + lparenthesis + ;
+        str = shape + space + lparenthesis + ;
         pattern.assign(shapep + spacep);
     }
     std::string showResult()
