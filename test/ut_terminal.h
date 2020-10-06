4a14ef7b90cce29ded3a1bd811c0abb98f4447ef
dell_wsl_ubuntu20.04 <t109598085@ntut.org.tw>
2020-10-06 23:57:35 +0800

    HW3_beta_0.03

--- test/ut_terminal.h
+++ test/ut_terminal.h
@@ -4,7 +4,7 @@
 #include "../src/rectangle.h"
 #include "../src/triangle.h"
 
-TEST(Terminal, test)
+TEST(Terminal, InvalidEllipse)
 {
     try
     {
@@ -21,4 +21,21 @@ TEST(Terminal, ignore)
 {
     Terminal terminal("Rectangle (2, 2) Rectangle (1, 1) Ellipse$%^&(1, 1) area inc");
     ASSERT_EQ("1.000\n4.000", terminal.showResult());
+}
+
+TEST(Terminal, AreaIncreasing)
+{
+    Terminal terminal("Rectangle (3.7, 4.2) Ellipse (4, 3) Triangle ([0,0], [-3,0], [0,-4]) area inc");
+    ASSERT_EQ("6.000\n15.540\n37.699", terminal.showResult());
+}
+TEST(Terminal, AreaDecreasing)
+{
+    Terminal terminal("Rectangle (3.7, 4.2) Ellipse (4, 3) Triangle ([0,0], [-3,0], [0,-4]) area dec");
+    ASSERT_EQ("37.699\n15.540\n6.000", terminal.showResult());
+}
+
+TEST(Terminal, AreaIncreasingWithOneShapeErrorArgumentIgnore)
+{
+    Terminal terminal("Rectangle (3.7, 4.2) Ellipse (4, 3) Triangle ([0,0], [-3,0], [0,-4]) Ellipse (four, 3) area inc");
+    ASSERT_EQ("6.000\n15.540\n37.699", terminal.showResult());
 }
