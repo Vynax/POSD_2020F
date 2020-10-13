e41041ed39e54b8cd0887fff84b5d01c8b6d9f05
dell_wsl_ubuntu20.04 <t109598085@ntut.org.tw>
2020-10-13 23:35:56 +0800

    HW4_beta_0.08

--- test/ut_compound_shape.h
+++ test/ut_compound_shape.h
@@ -4,12 +4,12 @@ class CompoundTest : public ::testing::Test
 protected:
     void SetUp() override
     {
-        r34 = new Rectangle("0", 3, 4);
-        e43 = new Ellipse("1", 4, 3);
+        r34 = new Rectangle("0", 3, 4, "yellow");
+        e43 = new Ellipse("1", 4, 3, "blue");
         triangleVector.push_back(new TwoDimensionalCoordinate(0, 0));
         triangleVector.push_back(new TwoDimensionalCoordinate(3, 0));
         triangleVector.push_back(new TwoDimensionalCoordinate(0, 4));
-        t345 = new Triangle("2", triangleVector);
+        t345 = new Triangle("2", triangleVector, "red");
     }
 
     void TearDown() override
@@ -27,10 +27,10 @@ protected:
 };
 TEST_F(CompoundTest, Constructor)
 {
-    std::vector<Shape *> shapesVector;
-    shapesVector.push_back(t345);
-    shapesVector.push_back(r34);
-    shapesVector.push_back(e43);
+    std::vector<Shape *> shapesVector = {t345, r34, e43};
+    // shapesVector.push_back(t345);
+    // shapesVector.push_back(r34);
+    // shapesVector.push_back(e43);
     CompoundShape cs("0", &shapesVector);
     //Shape *shapes[3] = {t345, r34, e43};
     EXPECT_EQ(6, shapesVector.at(0)->area());
@@ -69,4 +69,49 @@ TEST_F(CompoundTest, Info)
     CompoundShape cs("0", &shapesVector);
     //Shape *shapes[3] = {t345, r34, e43};
     EXPECT_EQ("Compound Shape {Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000]), Rectangle (3.000, 4.000), Ellipse (4.000, 3.000)}", cs.info());
+}
+
+TEST_F(CompoundTest, Delete)
+{
+    std::vector<Shape *> shapesVector;
+    shapesVector.push_back(t345);
+    shapesVector.push_back(r34);
+    shapesVector.push_back(e43);
+    CompoundShape cs("0", &shapesVector);
+    //Shape *shapes[3] = {t345, r34, e43};
+    EXPECT_EQ("Compound Shape {Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000]), Rectangle (3.000, 4.000), Ellipse (4.000, 3.000)}", cs.info());
+    cs.deleteShapeById("0");
+    EXPECT_EQ("Compound Shape {Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000]), Ellipse (4.000, 3.000)}", cs.info());
+}
+
+TEST_F(CompoundTest, DeleteInTripleLevel)
+{
+    std::vector<Shape *> shapesVector = {t345, r34, e43};
+    CompoundShape cs1("s_0_1_2", &shapesVector);
+    std::vector<Shape *> shapesV2 = {new Rectangle("12", 2.000, 6.000, "green"), new Rectangle("25", 5.000, 5.000, "white")};
+    CompoundShape cs2("s_12_25", &shapesV2);
+    std::vector<Shape *> shapesV3 = {&cs1, &cs2};
+    CompoundShape cs3("cs3", &shapesV3);
+    EXPECT_EQ("Compound Shape {Compound Shape {Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000]), Rectangle (3.000, 4.000), Ellipse (4.000, 3.000)}, Compound Shape {Rectangle (2.000, 6.000), Rectangle (5.000, 5.000)}}", cs3.info());
+    cs3.deleteShapeById("12");
+    EXPECT_EQ("Compound Shape {Compound Shape {Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000]), Rectangle (3.000, 4.000), Ellipse (4.000, 3.000)}, Compound Shape {Rectangle (5.000, 5.000)}}", cs3.info());
+}
+
+TEST_F(CompoundTest, Exception)
+{
+    std::vector<Shape *> shapesVector;
+    // shapesVector.push_back(t345);
+    // shapesVector.push_back(r34);
+    // shapesVector.push_back(e43);
+    try
+    {
+        CompoundShape cs("0", &shapesVector); //==> the argument should > 0 or the rectangle can't be build.
+        FAIL();
+    }
+    catch (std::string e)
+    {
+        EXPECT_EQ("This is not a compound shape!", e);
+    }
+
+    //Shape *shapes[3] = {t345, r34, e43};
 }
