19c0f80036cf08df2941ea5f421a38347f7e3d5a
dell_wsl_ubuntu20.04 <t109598085@ntut.org.tw>
2020-11-30 23:36:43 +0800

    HW6_shape_builder_ok
    shape_builder測試還沒寫

--- test/ut_scanner.h
+++ test/ut_scanner.h
@@ -4,12 +4,12 @@ TEST(Scanner, Ellipse)
 {
     Scanner scanner("Ellipse (4.000, 3.000)");
 
-    ASSERT_EQ("Ellipse", scanner.nextToken());
-    ASSERT_EQ("(", scanner.nextToken());
-    ASSERT_EQ("4.000", scanner.nextToken());
-    ASSERT_EQ(",", scanner.nextToken());
-    ASSERT_EQ("3.000", scanner.nextToken());
-    ASSERT_EQ(")", scanner.nextToken());
+    EXPECT_EQ("Ellipse", scanner.nextToken());
+    EXPECT_EQ("(", scanner.nextToken());
+    EXPECT_EQ("4.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("3.000", scanner.nextToken());
+    EXPECT_EQ(")", scanner.nextToken());
 
     try
     {
@@ -18,7 +18,7 @@ TEST(Scanner, Ellipse)
     }
     catch (std::string e)
     {
-        ASSERT_EQ("next char doesn't exist.", e);
+        EXPECT_EQ("next char doesn't exist.", e);
     }
 }
 
@@ -26,12 +26,12 @@ TEST(Scanner, Rectangle)
 {
     Scanner scanner("Rectangle (5.000, 3.000)");
 
-    ASSERT_EQ("Rectangle", scanner.nextToken());
-    ASSERT_EQ("(", scanner.nextToken());
-    ASSERT_EQ("5.000", scanner.nextToken());
-    ASSERT_EQ(",", scanner.nextToken());
-    ASSERT_EQ("3.000", scanner.nextToken());
-    ASSERT_EQ(")", scanner.nextToken());
+    EXPECT_EQ("Rectangle", scanner.nextToken());
+    EXPECT_EQ("(", scanner.nextToken());
+    EXPECT_EQ("5.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("3.000", scanner.nextToken());
+    EXPECT_EQ(")", scanner.nextToken());
 
     try
     {
@@ -40,7 +40,7 @@ TEST(Scanner, Rectangle)
     }
     catch (std::string e)
     {
-        ASSERT_EQ("next char doesn't exist.", e);
+        EXPECT_EQ("next char doesn't exist.", e);
     }
 }
 
@@ -48,20 +48,20 @@ TEST(Scanner, Triangle)
 {
     Scanner scanner("Triangle(0.000, 0.000, 0.000, -3.000, -4.000, 0.000)");
 
-    ASSERT_EQ("Triangle", scanner.nextToken());
-    ASSERT_EQ("(", scanner.nextToken());
-    ASSERT_EQ("0.000", scanner.nextToken());
-    ASSERT_EQ(",", scanner.nextToken());
-    ASSERT_EQ("0.000", scanner.nextToken());
-    ASSERT_EQ(",", scanner.nextToken());
-    ASSERT_EQ("0.000", scanner.nextToken());
-    ASSERT_EQ(",", scanner.nextToken());
-    ASSERT_EQ("-3.000", scanner.nextToken());
-    ASSERT_EQ(",", scanner.nextToken());
-    ASSERT_EQ("-4.000", scanner.nextToken());
-    ASSERT_EQ(",", scanner.nextToken());
-    ASSERT_EQ("0.000", scanner.nextToken());
-    ASSERT_EQ(")", scanner.nextToken());
+    EXPECT_EQ("Triangle", scanner.nextToken());
+    EXPECT_EQ("(", scanner.nextToken());
+    EXPECT_EQ("0.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("0.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("0.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("-3.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("-4.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("0.000", scanner.nextToken());
+    EXPECT_EQ(")", scanner.nextToken());
 
     try
     {
@@ -70,6 +70,63 @@ TEST(Scanner, Triangle)
     }
     catch (std::string e)
     {
-        ASSERT_EQ("next char doesn't exist.", e);
+        EXPECT_EQ("next char doesn't exist.", e);
+    }
+}
+
+TEST(Scanner, Compound_Shape)
+{
+    Scanner scanner("Compound Shape { Triangle([ 0.000, 0.000 ], [ 3.000, 0.000 ], [ 0.000, 4.000 ]), Rectangle(3.000, 4.000), Ellipse(4.000, 3.000) }");
+
+    EXPECT_EQ("Compound", scanner.nextToken());
+    EXPECT_EQ("Shape", scanner.nextToken());
+    EXPECT_EQ("{", scanner.nextToken());
+
+    EXPECT_EQ("Triangle", scanner.nextToken());
+    EXPECT_EQ("(", scanner.nextToken());
+    EXPECT_EQ("[", scanner.nextToken());
+    EXPECT_EQ("0.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("0.000", scanner.nextToken());
+    EXPECT_EQ("]", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("[", scanner.nextToken());
+    EXPECT_EQ("3.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("0.000", scanner.nextToken());
+    EXPECT_EQ("]", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("[", scanner.nextToken());
+    EXPECT_EQ("0.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("4.000", scanner.nextToken());
+    EXPECT_EQ("]", scanner.nextToken());
+    EXPECT_EQ(")", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+
+    EXPECT_EQ("Rectangle", scanner.nextToken());
+    EXPECT_EQ("(", scanner.nextToken());
+    EXPECT_EQ("3.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("4.000", scanner.nextToken());
+    EXPECT_EQ(")", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+
+    EXPECT_EQ("Ellipse", scanner.nextToken());
+    EXPECT_EQ("(", scanner.nextToken());
+    EXPECT_EQ("4.000", scanner.nextToken());
+    EXPECT_EQ(",", scanner.nextToken());
+    EXPECT_EQ("3.000", scanner.nextToken());
+    EXPECT_EQ(")", scanner.nextToken());
+
+    EXPECT_EQ("}", scanner.nextToken());
+    try
+    {
+        scanner.nextToken();
+        FAIL();
+    }
+    catch (std::string e)
+    {
+        EXPECT_EQ("next char doesn't exist.", e);
     }
 }
--- test/ut_shape_builder.h
+++ test/ut_shape_builder.h
@@ -1,11 +1,31 @@
 #include "../src/shape_builder.h"
 
-TEST(ShapeBuilderTest, build_triangle)
+TEST(ShapeBuilderTest, build_Triangle)
 {
     ShapeBuilder sb;
     sb.buildTriangle(0, 0, 0, -3, -4, 0);
     std::deque<Shape *> results = sb.getResult();
 
-    ASSERT_EQ(1, results.size());
-    ASSERT_EQ("Triangle ([0.000, 0.000], [0.000, -3.000], [-4.000, 0.000])", results[0]->info());
+    EXPECT_EQ(1, results.size());
+    EXPECT_EQ("Triangle ([0.000, 0.000], [0.000, -3.000], [-4.000, 0.000])", results[0]->info());
+}
+
+TEST(ShapeBuilderTest, build_Ellipse)
+{
+    ShapeBuilder sb;
+    sb.buildEllipse(4.000, 3.000);
+    std::deque<Shape *> results = sb.getResult();
+
+    EXPECT_EQ(1, results.size());
+    EXPECT_EQ("Ellipse (4.000, 3.000)", results[0]->info());
+}
+
+TEST(ShapeBuilderTest, build_Rectangle)
+{
+    ShapeBuilder sb;
+    sb.buildRectangle(5.000, 3.000);
+    std::deque<Shape *> results = sb.getResult();
+
+    EXPECT_EQ(1, results.size());
+    EXPECT_EQ("Rectangle (5.000, 3.000)", results[0]->info());
 }
