d3fc9f512bf8061ac57e03f527047f9ba3772b7b
midterm2 <t109598085@ntut.org.tw>
2020-12-09 21:52:40 +0800

    midterm2_not_finished

--- src/node_builder.h
+++ src/node_builder.h
@@ -1,5 +1,8 @@
 #include <deque>
 #include "node.h"
+#include <stack>
+class App;
+class Folder;
 
 class NodeBuilder
 {
@@ -7,6 +10,8 @@ public:
     void buildApp(std::string name, double size)
     {
         // build a app with an unique id and push in a std::stack.
+        _stack.push(new App(std::to_string(id), name, size));
+        id++;
     }
 
     void buildFolderBegin(std::string name)
@@ -23,6 +28,16 @@ public:
     {
         // return result.
         std::deque<Node *> result;
-        return result;
+        // return result.
+        while (!_stack.empty())
+        {
+            result.push_back(_stack.top());
+            _stack.pop();
+        }
+        return std::deque<Node *>(result.rbegin(), result.rend());
     }
+
+private:
+    std::stack<Node *> _stack;
+    int id = 0;
 };
